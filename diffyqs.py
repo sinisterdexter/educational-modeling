886e86b8c025cd0208ef536d09347c5e025b9bf4
Alex Davis <ecolecule@gmail.com>
2012-10-15 23:10:55 -0400

    Made a class representing a system of linear equations

---
+++ diffyqs.py
@@ -0,0 +1,29 @@
+from __future__ import division
+import csv
+
+class State(object):
+    def __init__(self, x0, y0, f, g):
+        self.x = x0
+        self.y = y0
+        self.f = f
+        self.g = g
+    
+    def update(self, h):
+        self.x = self.x + f(self.x, self.y)*h
+        self.y = self.y + g(self.x, self.y)*h
+        return (self.x, self.y)
+    
+    def write(self, t, h, output_path):
+        '''Write a csv file of the dynamics over time t, calculated using
+        time intervals of h, to output_path'''
+
+        # Figure out how many iterations to do
+        n = t/h
+
+        # Write the iterations to a csv file
+        with open(output_path, 'wb') as f:
+            writer = csv.writer(f)
+            # Write initial state:
+            csv.writer(f).writerow((self.x, self.y))
+            # Write the next n states:
+            csv.writer(f).writerows(self.update(h) for i in xrange(n))
